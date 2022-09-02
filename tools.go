b3bbc80ee9a202282dcd5ce23ff392a63367ff20
Gustavo Aquino <gustyaquino@gmail.com>
2022-09-02 16:24:39 -0400

    Added Creating of Dirs Function

--- tools.go
+++ tools.go
@@ -70,7 +70,13 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 		t.MaxFileSize = 1024 * 1024 * 1024
 	}
 
-	err := r.ParseMultipartForm(int64(t.MaxFileSize))
+	err := t.CreateDirIfNotExists(uploadDir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.ParseMultipartForm(int64(t.MaxFileSize))
 
 	if err != nil {
 		return nil, errors.New("the uploaded file is too big")
@@ -157,4 +163,17 @@ func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) (
 	}
 
 	return uploadedFiles, nil
+}
+
+// CreateDirIfNotExists creates a directory, and all necessary parents, if it does not exists
+func (t *Tools) CreateDirIfNotExists(path string) error {
+	const mode = 0755
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
